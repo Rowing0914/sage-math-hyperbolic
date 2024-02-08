e5fbf3959906182b0f9acd5eaa82c6f0ce3d60c0
unknown <jp30695@linecorp.com>
2024-02-08 21:22:52 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # sage-math-hyperbolic
+
+### Background
+This time, we implemented it using SageMath, a scientific computation library available in Python (programming language) that consists of a collection of pre-implemented functions.
+
+### How to use it
+When you open the URLs above, after waiting a bit, something like the image shown below will appear at the bottom of the page (the image is of the Coxeter 4-gon).
+** At the top of the page, you'll find the script I wrote.
+You should enter the values for the x, y coordinates of the Base-point and press Enter. Then, the script will run again, and the visualization will be updated.
+
+### Demo
+![Coxeter 4-gon](./demo.png)
---
+++ triangle_gp.py
@@ -0,0 +1,108 @@
+PD = HyperbolicPlane().PD()
+
+# === Triangle group
+l1 = PD.get_geodesic(.25 + .25 * I, -.25 + .25 * I)
+l2 = PD.get_geodesic(-.25 + .25 * I, -.25 * I)
+l3 = PD.get_geodesic(-.25 * I, .25 + .25 * I)
+
+@interact
+def _(x=0.0, y=0.0):
+	# base point
+	p_base = PD.get_point(x + y*I)
+
+	# # p_base = PD.get_point(0.0 + 0.1 * I)  # centre
+	# p_base = PD.get_point(0.234*I)  # on M1
+
+	# get 1st reflection transformations
+	R1 = l1.reflection_involution()
+	R2 = l2.reflection_involution()
+	R3 = l3.reflection_involution()
+
+	# reflection in l1
+	R1_l1 = R1*l1
+	R1_l2 = R1*l2
+	R1_l3 = R1*l3
+	# reflection in l2
+	R2_l1 = R2*l1
+	R2_l2 = R2*l2
+	R2_l3 = R2*l3
+	# reflection in l3
+	R3_l1 = R3*l1
+	R3_l2 = R3*l2
+	R3_l3 = R3*l3
+
+	# reflect base point
+	R1_pBase = R1*p_base
+	R2_pBase = R2*p_base
+	R3_pBase = R3*p_base
+
+	# get 2nd reflection transformations
+	R2R1 = R1_l2.reflection_involution()
+	R3R1 = R1_l3.reflection_involution()
+	R1R2 = R2_l1.reflection_involution()
+	R3R2 = R2_l3.reflection_involution()
+	R1R3 = R3_l1.reflection_involution()
+	R2R3 = R3_l2.reflection_involution()
+
+	# reflect base point once more
+	R2R1_pBase = R2R1*R1_pBase
+	R3R1_pBase = R3R1*R1_pBase
+	R1R2_pBase = R1R2*R2_pBase
+	R3R2_pBase = R3R2*R2_pBase
+	R1R3_pBase = R1R3*R3_pBase
+	R2R3_pBase = R2R3*R3_pBase
+
+	# reflect all sides once more
+	R2R1_l1 = R2R1*R1_l1
+	R2R1_l2 = R2R1*R1_l2
+	R2R1_l3 = R2R1*R1_l3
+	R3R1_l1 = R3R1*R1_l1
+	R3R1_l2 = R3R1*R1_l2
+	R3R1_l3 = R3R1*R1_l3
+
+	R1R2_l1 = R1R2*R2_l1
+	R1R2_l2 = R1R2*R2_l2
+	R1R2_l3 = R1R2*R2_l3
+	R3R2_l1 = R3R2*R2_l1
+	R3R2_l2 = R3R2*R2_l2
+	R3R2_l3 = R3R2*R2_l3
+
+	R1R3_l1 = R1R3*R3_l1
+	R1R3_l2 = R1R3*R3_l2
+	R1R3_l3 = R1R3*R3_l3
+	R2R3_l1 = R2R3*R3_l1
+	R2R3_l2 = R2R3*R3_l2
+	R2R3_l3 = R2R3*R3_l3
+
+	# P-bisectors b/w 2nd reflections and base point
+	l_pBase_R2R1base = PD.get_geodesic(p_base, R2R1_pBase).perpendicular_bisector().complete()
+	l_pBase_R3R1base = PD.get_geodesic(p_base, R3R1_pBase).perpendicular_bisector().complete()
+	l_pBase_R1R2base = PD.get_geodesic(p_base, R1R2_pBase).perpendicular_bisector().complete()
+	l_pBase_R3R2base = PD.get_geodesic(p_base, R3R2_pBase).perpendicular_bisector().complete()
+	l_pBase_R1R3base = PD.get_geodesic(p_base, R1R3_pBase).perpendicular_bisector().complete()
+	l_pBase_R2R3base = PD.get_geodesic(p_base, R2R3_pBase).perpendicular_bisector().complete()
+
+	# Plot sides
+	P = l1.plot() + l2.plot() + l3.plot()
+	# plot base point
+	P += p_base.show(size=50, color="blue", legend_label='p-base')
+	# plot reflected sides
+	P += R1_l2.plot() + R1_l3.plot()
+	P += R2_l1.plot() + R2_l3.plot()
+	P += R3_l1.plot() + R3_l2.plot()
+	# plot 1st reflection of points
+	P += R1_pBase.show(color="red") + R2_pBase.show(color="red") + R3_pBase.show(color="red")
+	# plot 2nd reflection of sides
+	# P += R2R1_l1.plot() + R2R1_l2.plot() + R2R1_l3.plot() + R3R1_l1.plot() + R3R1_l2.plot() + R3R1_l3.plot()
+	# P += R1R2_l1.plot() + R1R2_l2.plot() + R1R2_l3.plot() + R3R2_l1.plot() + R3R2_l2.plot() + R3R2_l3.plot()
+	# P += R1R3_l1.plot() + R1R3_l2.plot() + R1R3_l3.plot() + R2R3_l1.plot() + R2R3_l2.plot() + R2R3_l3.plot()
+	# # plot 2nd reflection of points
+	P += R2R1_pBase.show(color="red") + R3R1_pBase.show(color="red")
+	P += R1R2_pBase.show(color="red") + R3R2_pBase.show(color="red")
+	P += R1R3_pBase.show(color="red") + R2R3_pBase.show(color="red")
+	# # plot p-bisectors for 2nd reflections
+	P += l_pBase_R2R1base.plot(thickness=2, color="orange") + l_pBase_R3R1base.plot(thickness=2, color="orange")
+	P += l_pBase_R1R2base.plot(thickness=2, color="orange") + l_pBase_R3R2base.plot(thickness=2, color="orange")
+	P += l_pBase_R1R3base.plot(thickness=2, color="orange") + l_pBase_R2R3base.plot(thickness=2, color="orange")
+
+	P.show(axes=True)
