4fda4f4db523139ebccfb53c1f1568861f4239b3
unknown <jp30695@linecorp.com>
2024-04-15 14:56:04 +0900

    update

---
+++ n_gon_wh_basePolygon.py
@@ -0,0 +1,247 @@
+import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
+import numpy as np
+from sage.plot.hyperbolic_polygon import HyperbolicPolygon
+
+base_polygon = HyperbolicPolygon([0.622152, 0.545613 * I, -0.622152, -0.545613 * I], model="PD", options={})
+
+PD = HyperbolicPlane().PD()
+UHP = HyperbolicPlane().UHP()
+
+""" Note
+Float-precision is crucial  to make computation of Dirichlet Domain work...
+But, the following precision causes the runtime error of computation of others when sides > 9...
+"""
+
+
+# CC = ComplexField(150)  # don't use this!
+# CC = ComplexField(20)  # don't use this!
+# RR = RealField(5)
+
+
+@cached_function
+def process_data(base_polygon, base_pt_x, base_pt_y):
+    # base point
+    p_base = PD.get_point(CC(base_pt_x + base_pt_y * I))
+    points = [PD.get_point(CC(p)) for p in base_polygon._pts]
+
+    n = int(len(points))
+
+    # define sides
+    sides = list()
+    for i in range(n):
+        if i + 1 == n:
+            _side = PD.get_geodesic(points[i], points[0])
+        else:
+            _side = PD.get_geodesic(points[i], points[i + 1])
+        sides.append(_side)
+
+    # # === Angle-Sum checker ===
+    for i in range(n):
+        if i + 1 == n:
+            angle = sides[i].angle(sides[0].complete())
+        else:
+            angle = sides[i].angle(sides[i + 1].complete())
+        print(f"Angle of vertex {i}: {np.round(a=np.rad2deg(float(angle)), decimals=2)}")
+
+    # Get 1st reflection transformations
+    reflection_1st = [l.reflection_involution() for l in sides]
+
+    # reflect the sides; fix R and iterate sides; [R1-l1, R1-l2, R1-l3, ...]
+    reflect_1st_sides = list()  # n^2
+    diff_reflection_index = list()
+    for i, R in enumerate(reflection_1st):  # n
+        for j, s in enumerate(sides):  # n
+            if i == j:
+                reflect_1st_sides.append(s)
+            else:
+                reflect_1st_sides.append(R * s)
+            _diff = abs(i - j)
+            _diff = 1 if _diff == (n - 1) else _diff  # modulo operation for cyclic ordering
+            diff_reflection_index.append(_diff)
+
+    # reflect base point: n
+    reflect_1st_pBase = [R * p_base for R in reflection_1st]
+
+    # get 2nd reflection transformations: n^2
+    reflection_2nd = [l.reflection_involution() for l in reflect_1st_sides]
+
+    # reflect all sides once more
+    reflect_2nd_sides = list()
+    reflect_2nd_pBase = list()
+    # reflection_2nd: [R1R1, R2R1, R3R1, ...] (n^2)
+    # reflect_1st_sides: [R1-l1, R1-l2, R1-l3, ...] (n^2)
+
+    # For Second reflection, eg., R_{i,j}, we compute Diff in i and j to differentiate colours in plot later.
+    diff_index = list()
+    for h in range(n):
+        for ind_i, i in enumerate(range(h * n, (h + 1) * n)):  # Apply reflection
+            for j in range(h * n, (h + 1) * n):  # side
+                _diff = diff_reflection_index[i]
+                R = reflection_2nd[i]
+                if i == j and ind_i != h:  # ind_i != h to avoid reflecting back to p-base
+                    # Transform point
+                    _p = reflect_1st_pBase[h]
+                    _p = R * _p
+                    reflect_2nd_pBase.append(_p)
+                    diff_index.append(_diff)
+                else:
+                    if i != j:
+                        # Transform sides
+                        _s = reflect_1st_sides[j]
+                        reflect_2nd_sides.append(R * _s)
+
+    # Sort by complex-argument
+    # reflect_2nd_pBase = sorted(reflect_2nd_pBase, key=lambda x: arg(x.coordinates()))
+
+    # P-bisectors b/w 2nd reflections and base point
+    list_perp_bisec = list()
+    for p in reflect_2nd_pBase:
+        _l = PD.get_geodesic(p_base, p).perpendicular_bisector().complete()
+        list_perp_bisec.append(_l)
+
+    # Compute all the intersections of perp-bisectors
+    intersect_p = list()
+    for i in range(len(list_perp_bisec)):
+        for j in range(i + 1, len(list_perp_bisec)):  # avoid the replicates
+            try:
+                _p = list_perp_bisec[i].intersection(list_perp_bisec[j])[0]
+                # _p = list_perp_bisec[j].intersection(list_perp_bisec[i])[0]
+
+                intersect_p.append(_p)
+            except Exception as e:
+                msg = str(e)
+                # print(msg)
+
+    # avoid the replicates; the above workaround sometime doesn't work so manually double-check
+    new_intersect_p = list()
+    for i in range(len(intersect_p)):
+        if_exist = False
+        p = intersect_p[i]
+        for j in range(i + 1, len(intersect_p)):
+            pp = intersect_p[j]
+            # print(p.dist(pp))
+            # if bool(_p.dist(__p) < 0.04):
+            if bool(p.dist(pp) < 0.01):  # this value corresponds to float-pt precision
+                # if bool(p.dist(pp) < 10**-6):  # this value corresponds to float-pt precision
+                if_exist = True
+                break
+        if not if_exist:
+            new_intersect_p.append(p)
+    intersect_p = new_intersect_p
+
+    table_if_exterior = np.zeros((len(intersect_p), len(reflect_2nd_pBase))).astype(bool)
+    for i, p in enumerate(intersect_p):
+        for j, p_2nd in enumerate(reflect_2nd_pBase):
+            _d_p = p.dist(p_2nd)
+            _d_p_base = p.dist(p_base)
+            _check = bool(_d_p_base < _d_p) or bool((_d_p_base - _d_p) <= 10 ** -9)
+            # print(i, j, _d_p, _d_p_base, _check)
+            table_if_exterior[i, j] = _check
+    # print(table_if_exterior)
+    ind = [intersect_p[i].coordinates() for i, row in enumerate(table_if_exterior) if np.all(row)]
+    # ind = [intersect_p[i].coordinates() for i, row in enumerate(table_if_exterior)]
+    return p_base, sides, reflect_1st_sides, reflect_1st_pBase, reflect_2nd_sides, reflect_2nd_pBase, list_perp_bisec, diff_index, ind
+
+
+prev_base_pt_x = None
+prev_base_pt_y = None
+
+# caching the computation outcomes!
+base_pt_x, base_pt_y = 0.0, -0.0
+# base_pt_x, base_pt_y = 0.2, 0.2
+
+p_base, sides, reflect_1st_sides, reflect_1st_pBase, reflect_2nd_sides, reflect_2nd_pBase, list_perp_bisec, diff_index, ind = process_data(
+    base_polygon, base_pt_x, base_pt_y)
+
+
+@interact
+def _(base_pt_x=base_pt_x, base_pt_y=base_pt_y, auto_update=False,
+      if_plot_sides=True, if_plot_reflect_1st_sides=False, if_plot_reflect_1st_pBase=False,
+      if_plot_reflect_2nd_sides=False, if_plot_reflect_2nd_pBase=False, if_plot_perp_bisec=True,
+      if_show_dirichletDomain=True,
+      ):
+    global prev_centre_polygon_UHP, prev_num_sides, prev_i_angle, prev_base_pt_x, prev_base_pt_y
+    global p_base, sides, reflect_1st_sides, reflect_1st_pBase, reflect_2nd_sides, reflect_2nd_pBase, list_perp_bisec, diff_index, ind
+
+    if_rerun = base_pt_x != prev_base_pt_x or base_pt_y != prev_base_pt_y
+    if if_rerun:  # Check if num_sides has changed
+        # Update the previous value of num_sides
+        prev_base_pt_x = base_pt_x
+        prev_base_pt_y = base_pt_y
+
+        p_base, sides, reflect_1st_sides, reflect_1st_pBase, reflect_2nd_sides, reflect_2nd_pBase, list_perp_bisec, diff_index, ind = process_data(
+            base_polygon, base_pt_x, base_pt_y)
+
+    print(f"Found {len(ind)}-gon")
+
+    # plot base point
+    P = p_base.show(size=50, color="blue", legend_label='p-base')
+
+    # Plot sides
+    if if_plot_sides:
+        for i in sides:
+            P += i.plot()
+
+    # plot reflected sides
+    if if_plot_reflect_1st_sides:
+        for i in reflect_1st_sides:
+            if i not in sides:
+                P += i.plot(color="magenta")
+
+    # plot 1st reflection of points
+    if if_plot_reflect_1st_pBase:
+        for i in reflect_1st_pBase:
+            P += i.show(color="red")
+
+    # plot 2nd reflection of sides
+    if if_plot_reflect_2nd_sides:
+        for i in reflect_2nd_sides:
+            if i not in sides:  # todo: this is not working...
+                P += i.plot(color="purple")
+
+    # plot 2nd reflection of points
+    if if_plot_reflect_2nd_pBase:
+        for i in reflect_2nd_pBase:
+            P += i.show(color="red")
+
+    # plot p-bisectors for 2nd reflections
+    if if_plot_perp_bisec:
+        used_colors = dict()
+        cmap = plt.colormaps['tab10']
+        for i, _diff in zip(list_perp_bisec, diff_index):
+            _c = cmap(_diff)
+            _c = mcolors.to_hex(_c)
+            P += i.plot(color=_c)
+            _name = f"L_i_i+{_diff}"
+            if _name not in used_colors:
+                used_colors[_name] = _c
+
+    if if_show_dirichletDomain:
+        ind = sorted(ind, key=arg)  # sort the vertices by complex-argument for correct instantiation of polygon!
+        for p in ind:
+            p = PD.get_point(coordinates=p)
+            P += p.show(color="red", size=30)
+        # g = hyperbolic_polygon(pts=ind, model="PD", fill=True, alpha=0.2)
+        g = hyperbolic_polygon(pts=ind, model="PD", fill=True, alpha=0.0)
+        P += g.plot()
+
+    P.show(axes=True)
+
+    if if_plot_perp_bisec:
+        num_colors = len(used_colors)
+        fig, ax = plt.subplots(figsize=(6, 1))
+
+        # Display the custom colors in a table
+        for i, (key, color) in enumerate(used_colors.items()):
+            ax.add_patch(plt.Rectangle((i - 0.5, -0.5), 1, 1, color=color, ec='black'))
+            ax.text(i, 0, key, ha='center', va='center', color='black', fontsize=8)
+
+        # Remove axis ticks and set axis limits
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_xlim(-0.5, num_colors - 0.5)
+        ax.set_ylim(-0.5, 0.5)
+
+        # Show plot
+        plt.show()
--- prototype/draw_trig_givenAngles.py
+++ prototype/draw_trig_givenAngles.py
@@ -3,7 +3,11 @@ import numpy as np
 UHP = HyperbolicPlane().UHP()  # UHP is the upper half plane IH
 HM = HyperbolicPlane().HM()  # HM  is the hyperboloid model
 PD = HyperbolicPlane().PD()
-a1, a2, a3 = pi / 4, pi / 4, pi / 10  # given angles, we draw a hyperbolic triangle with these angles
+
+n1, n2, n3 = 4, 4, 8
+a1, a2, a3 = pi / n1, pi / n2, pi / n3  # given angles, we draw a hyperbolic triangle with these angles
+N = 2 * lcm([n1, n2, n3])
+F.<u> = CyclotomicField(N)
 
 
 def c(a1, a2, a3):
@@ -13,6 +17,16 @@ def c(a1, a2, a3):
 c1, c2, c3 = c(a1, a2, a3), c(a2, a3, a1), c(a3, a1, a2)  # algebraic in the given example
 
 
+# sides are CC(arccosh(c1)), CC(arccosh(c2)), CC(arccosh(c3))
+
+def toUHP(M):
+    """M is a point in the HM model, we construct an ad-hoc version in UHP
+    """
+    a, b, c = M.coordinates()
+    a, b, c = QQbar(a), QQbar(b), QQbar(c)
+    return UHP(-(a + i) * (c + 1) / ((b - 1) * c - a ^ 2 - b ^ 2 + b - 1))
+
+
 def s(v, w):
     """v, w are vectors with three entries, we return the Minkowski product with signature ++-"""
     return v * diagonal_matrix([1, 1, -1]) * w  # no need of a transposition
@@ -20,25 +34,23 @@ def s(v, w):
 
 # a, b, p, q, r are used in the following coordinates in Hyperboloid model
 # as a parametrization of points
-myvars = var("a b p q r")
-a, b, p, q, r = myvars
+R.<a,b,p,q,r> = PolynomialRing(F)
 
 V1 = vector([0, 0, 1])
 V2 = vector([0, a, b])
 V3 = vector([p, q, r])
 
-sols = solve([s(V2, V2) == -1, s(V3, V3) == -1,
-              s(V1, V2) == -c3, s(V2, V3) == -c1, s(V3, V1) == -c2]
-             , myvars, solution_dict=True)
-sols = [sol for sol in sols if sol[a] > 0 and sol[q] > 0]  # so V2, V3 maps to IH
+J = R.ideal([s(V2, V2) + 1, s(V3, V3) + 1,
+             s(V1, V2) + c3, s(V2, V3) + c1, s(V3, V1) + c2])
+V = J.variety(ring=QQbar)
+sols = [sol for sol in V if sol[a] > 0 and sol[q] > 0]  # so V2, V3 maps to IH
 sol = sols[0]  # first solution
 
-# a0, b0, p0, q0, r0 = [sol[v].simplify_full() for v in myvars]
-a0, b0, p0, q0, r0 = [CC(sol[v].simplify_full()) for v in myvars]
+a0, b0, p0, q0, r0 = [sol[v].real() for v in R.gens()]
 
 S1, S2, S3 = vector([0, 0, 1]), vector([0, a0, b0]), vector([p0, q0, r0])
 M1, M2, M3 = HM.get_point(S1), HM.get_point(S2), HM.get_point(S3)
-H1, H2, H3 = UHP(M1), UHP(M2), UHP(M3)  # using the coercion from HM to UHP
+H1, H2, H3 = toUHP(M1), toUHP(M2), toUHP(M3)  # using the ad-hoc coercion from HM to UHP
 P1, P2, P3 = PD(H1), PD(H2), PD(H3)
 Q1, Q2, Q3 = H1.coordinates(), H2.coordinates(), H3.coordinates()
 p1, p2, p3 = P1.coordinates(), P2.coordinates(), P3.coordinates()
@@ -54,6 +66,5 @@ print("Angles: ", round(_a1, ndigits=2), round(_a2, ndigits=2), round(_a3, ndigi
 p = hyperbolic_polygon(pts=[p1, p2, p3], model="PD", fill=True, alpha=0.3)
 
 g = Graphics()
-# g += p.plot()
 g += p.plot()
 g.show(axes=True, aspect_ratio=1)
